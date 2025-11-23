924967b9367f7956c530daa030834c1c9343d21c
carolnascgoncalves <carolrsc1411@gmail.com>
2025-11-22 23:10:30 -0300

    QUINTA tentativa com o CORS

--- src/main/java/br/com/fiap/config/CORSFilter.java
+++ src/main/java/br/com/fiap/config/CORSFilter.java
@@ -11,13 +11,19 @@ public class CORSFilter implements ContainerRequestFilter, ContainerResponseFilt
     @Override
     public void filter(ContainerRequestContext request) throws IOException {
         if (request.getMethod().equalsIgnoreCase("OPTIONS")) {
-            request.abortWith(Response.ok().build());
+            request.abortWith(
+                    Response.ok()
+                            .header("Access-Control-Allow-Origin", "http://localhost:5173")
+                            .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+                            .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+                            .build()
+            );
         }
     }
 
     @Override
     public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
-        response.getHeaders().add("Access-Control-Allow-Origin", "*");
+        response.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:5173");
         response.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
         response.getHeaders().add("Access-Control-Allow-Credentials", "true");
         response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
