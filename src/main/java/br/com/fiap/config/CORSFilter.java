e8aba8392f94628e4f012c69ace2773bf551debf
carolnascgoncalves <carolrsc1411@gmail.com>
2025-11-23 15:57:52 -0300

    SEXTA e se Deus quiser, ultima tentativa com CORS

--- src/main/java/br/com/fiap/config/CORSFilter.java
+++ src/main/java/br/com/fiap/config/CORSFilter.java
@@ -7,15 +7,36 @@ import java.io.IOException;
 
 @Provider
 public class CORSFilter implements ContainerRequestFilter, ContainerResponseFilter {
+    private static final String API_KEY = System.getenv("API_KEY");
 
     @Override
     public void filter(ContainerRequestContext request) throws IOException {
         if (request.getMethod().equalsIgnoreCase("OPTIONS")) {
             request.abortWith(
                     Response.ok()
-                            .header("Access-Control-Allow-Origin", "http://localhost:5173")
-                            .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+                            .header("Access-Control-Allow-Origin", "*")
+                            .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, x-api-key")
                             .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+                            .build()
+            );
+            return;
+        }
+
+        String headerKey = request.getHeaderString("x-api-key");
+
+        if (API_KEY == null || API_KEY.isBlank()) {
+            request.abortWith(
+                    Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                            .entity("API_KEY não configurada no servidor.")
+                            .build()
+            );
+            return;
+        }
+
+        if (headerKey == null || !headerKey.equals(API_KEY)) {
+            request.abortWith(
+                    Response.status(Response.Status.UNAUTHORIZED)
+                            .entity("Acesso negado: x-api-key inválida ou ausente.")
                             .build()
             );
         }
@@ -23,9 +44,8 @@ public class CORSFilter implements ContainerRequestFilter, ContainerResponseFilt
 
     @Override
     public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
-        response.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:5173");
-        response.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
-        response.getHeaders().add("Access-Control-Allow-Credentials", "true");
-        response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        response.getHeaders().putSingle("Access-Control-Allow-Origin", "*");
+        response.getHeaders().putSingle("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, x-api-key");
+        response.getHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
     }
 }
